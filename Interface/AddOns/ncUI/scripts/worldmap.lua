b6dcf1babd8358ab5a1c715a9a403dfc68ee9040
unknown <Orson Peters@.(none)>
2010-04-04 18:22:08 +0200

    changes, I love them!

--- Interface/AddOns/ncUI/scripts/worldmap.lua
+++ Interface/AddOns/ncUI/scripts/worldmap.lua
@@ -21,7 +21,7 @@ SetCVar("miniWorldMap", 1)
 SetCVar("advancedWorldMap", 1)
 
 
-local SmallerMapSkin = function()
+hooksecurefunc("WorldMap_ToggleSizeDown", function()
 	F:Destroy(WorldMapBlobFrame)
 	F:Destroy(WorldMapFrameSizeUpButton) -- NO SIZING UP MWUHAHAHAA
 	
@@ -33,34 +33,38 @@ local SmallerMapSkin = function()
 	mapbg:SetPoint("BOTTOMRIGHT", WorldMapDetailFrame, 2, -2)
 	mapbg:SetFrameStrata("MEDIUM")
 	mapbg:SetFrameLevel(20)
-	
-	
+
 	-- move buttons / texts and hide default border
 	WorldMapButton:SetAllPoints(WorldMapDetailFrame)
+
 	WorldMapFrame:SetFrameStrata("MEDIUM")
 	WorldMapFrame:SetClampedToScreen(true) 
 	WorldMapDetailFrame:SetFrameStrata("MEDIUM")
+	
 	WorldMapTitleButton:Show()	
+
 	WorldMapFrameMiniBorderLeft:Hide()
 	WorldMapFrameMiniBorderRight:Hide()
-	WorldMapFrameSizeUpButton:Show()
-	WorldMapFrameSizeUpButton:ClearAllPoints()
-	WorldMapFrameSizeUpButton:SetPoint("TOPRIGHT", WorldMapButton, "TOPRIGHT", F:Scale(3), F:Scale(-18))
-	WorldMapFrameSizeUpButton:SetFrameStrata("HIGH")
+	
+	F:SetToolbox(WorldMapFrameCloseButton, WorldMapQuestShowObjectives, WorldMapQuestShowObjectivesText, WorldMapFrameTitle)
+	
 	WorldMapFrameCloseButton:ClearAllPoints()
-	WorldMapFrameCloseButton:SetPoint("TOPRIGHT", WorldMapButton, "TOPRIGHT", F:Scale(3), F:Scale(3))
+	WorldMapFrameCloseButton:SetPoint("TOPRIGHT", WorldMapButton, "TOPRIGHT", 3, 3)
 	WorldMapFrameCloseButton:SetFrameStrata("HIGH")
-	WorldMapFrameSizeDownButton:SetPoint("TOPRIGHT", WorldMapFrameMiniBorderRight, "TOPRIGHT", F:Scale(-66), F:Scale(5))
+	
 	WorldMapQuestShowObjectives:SetParent(ald)
 	WorldMapQuestShowObjectives:ClearAllPoints()
-	WorldMapQuestShowObjectives:SetPoint("BOTTOMRIGHT", WorldMapButton, "BOTTOMRIGHT", 0, F:Scale(-1))
+	WorldMapQuestShowObjectives:SetPoint("BOTTOMRIGHT", WorldMapButton, "BOTTOMRIGHT", 0, -1)
+	
 	WorldMapQuestShowObjectivesText:SetFontObject("ncUIfont")
 	WorldMapQuestShowObjectivesText:ClearAllPoints()
-	WorldMapQuestShowObjectivesText:SetPoint("RIGHT", WorldMapQuestShowObjectives, "LEFT", F:Scale(-4), F:Scale(1))
+	WorldMapQuestShowObjectivesText:SetPoint("RIGHT", WorldMapQuestShowObjectives, "LEFT", -4, 1)
+	
 	WorldMapFrameTitle:ClearAllPoints()
-	WorldMapFrameTitle:SetPoint("BOTTOMLEFT", WorldMapDetailFrame, F:Scale(9), F:Scale(5));
+	WorldMapFrameTitle:SetPoint("BOTTOMLEFT", WorldMapDetailFrame, 9, 5)
 	WorldMapFrameTitle:SetFontObject("ncUIfont")
-	WorldMapFrameTitle:SetParent(ald)		
+	WorldMapFrameTitle:SetParent(ald)
+
 	WorldMapTitleButton:SetFrameStrata("TOOLTIP")
 	WorldMapTooltip:SetFrameStrata("TOOLTIP")
 	
@@ -72,54 +76,42 @@ local SmallerMapSkin = function()
 	WorldMapQuestPOI_OnLeave = function()
 		WorldMapTooltip:Hide()
 	end
-end
-hooksecurefunc("WorldMap_ToggleSizeDown", function() SmallerMapSkin() end)
+end)
 
 local function OnMouseDown()
-	WorldMapScreenAnchor:ClearAllPoints();
-	WorldMapFrame:ClearAllPoints();
-	WorldMapFrame:StartMoving(); 
+	WorldMapScreenAnchor:ClearAllPoints()
+	WorldMapFrame:ClearAllPoints()
+	WorldMapFrame:StartMoving() 
 end
 
 local function OnMouseUp()
-	WorldMapFrame:StopMovingOrSizing();
-	WorldMapScreenAnchor:StartMoving();
-	WorldMapScreenAnchor:SetPoint("TOPLEFT", WorldMapFrame);
-	WorldMapScreenAnchor:StopMovingOrSizing();
+	WorldMapFrame:StopMovingOrSizing()
+	WorldMapScreenAnchor:StartMoving()
+	WorldMapScreenAnchor:SetPoint("TOPLEFT", WorldMapFrame)
+	WorldMapScreenAnchor:StopMovingOrSizing()
 end
 
 movebutton:EnableMouse(true)
 movebutton:SetScript("OnMouseDown", OnMouseDown)
 movebutton:SetScript("OnMouseUp", OnMouseUp)
 
--- the classcolor function
 local function UpdateIconColor(self, t)
 	color = RAID_CLASS_COLORS[select(2, UnitClass(self.unit))]
 	self.icon:SetVertexColor(color.r, color.g, color.b)
 end
 
-
 addon:SetScript("OnEvent", function()
-	-- fixing a stupid bug error by blizzard on default ui :x
-	if event == "PLAYER_REGEN_DISABLED" then
-		WorldMapFrameSizeDownButton:Disable() 
-		WorldMapFrameSizeUpButton:Disable()
-	elseif event == "PLAYER_REGEN_ENABLED" then
-		WorldMapFrameSizeDownButton:Enable()
-		WorldMapFrameSizeUpButton:Enable()
-	else
-		for r=1, 40 do
-			if UnitInParty(_G["WorldMapRaid"..r].unit) then
-				_G["WorldMapRaid"..r].icon:SetTexture("Interface\\AddOns\\Tukui\\media\\Party")
-			else
-				_G["WorldMapRaid"..r].icon:SetTexture("Interface\\AddOns\\Tukui\\media\\Raid")
-			end
-			_G["WorldMapRaid"..r]:SetScript("OnUpdate", UpdateIconColor)
+	for r=1, 40 do
+		if UnitInParty(_G["WorldMapRaid"..r].unit) then
+			_G["WorldMapRaid"..r].icon:SetTexture([[Interface\AddOns\ncUI\media\party]])
+		else
+			_G["WorldMapRaid"..r].icon:SetTexture([[Interface\AddOns\ncUI\media\raid]])
 		end
+		_G["WorldMapRaid"..r]:SetScript("OnUpdate", UpdateIconColor)
+	end
 
-		for p=1, 4 do
-			_G["WorldMapParty"..p].icon:SetTexture("Interface\\AddOns\\Tukui\\media\\Party")
-			_G["WorldMapParty"..p]:SetScript("OnUpdate", UpdateIconColor)
-		end
+	for p=1, 4 do
+		_G["WorldMapParty"..p].icon:SetTexture([[Interface\AddOns\ncUI\media\party]])
+		_G["WorldMapParty"..p]:SetScript("OnUpdate", UpdateIconColor)
 	end
 end)
